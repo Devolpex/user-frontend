9a0a7ed49d4e778e0321452391c4d18c46fb25cc
marouanedbibih <dbibih.marouane@emsi-edu.ma>
2024-04-05 21:59:32 +0000

    Develop client table

--- src/views/client/ClientList.jsx
+++ src/views/client/ClientList.jsx
@@ -1,9 +1,24 @@
 // eslint-disable-next-line no-unused-vars
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import ClientTable from '../../components/table/ClientTable'
+import clientInfos from '../../../test/data/clients.json'
 
 function ClientList() {
+  const [clients, setClients] = useState([])
+  const [loading, setLoading] = useState(false)
+
+  useEffect(() => {
+    setClients(clientInfos)
+  }, [clientInfos])
+  console.log("Client Infos",clients);
+
+
+  const onDeleteClick = (client) => {}
   return (
-    <div>UserList</div>
+    <div className='w-full flex flex-col justify-center items-center gap-8'>
+      UserList
+      <ClientTable clients={clients} loading={loading} onDeleteClick={onDeleteClick}/>
+    </div>
   )
 }
 
